a95bbdcce2811b8596d87ddab7738a12de702751
JulianCazaux <136473755+JulianCazaux@users.noreply.github.com>
2024-05-30 18:41:14 -0300

    chore: generate card resources and connect with prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CardModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/card/card.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCardDto } from './dto/create-card.dto';
+import { UpdateCardDto } from './dto/update-card.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CardService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createCardDto: CreateCardDto) {
+    return 'This action adds a new card';
+  }
+
+  async findAll() {
+    return this.prisma.card.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} card`;
+  }
+
+  update(id: number, updateCardDto: UpdateCardDto) {
+    return `This action updates a #${id} card`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} card`;
+  }
+}
