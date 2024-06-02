de96418b4bcd423f2b9f6de2a412d26f61e240dc
JulianCazaux <136473755+JulianCazaux@users.noreply.github.com>
2024-06-02 18:25:19 -0300

    chore: unit test for create and findOne methods in cards controller

--- backend/src/cards/cards.controller.spec.ts
+++ backend/src/cards/cards.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
+import { CreateCardDto } from './dto/create-card.dto';
+
+const cardId = 345;
 
 describe('CardsController', () => {
   let controller: CardsController;
@@ -8,7 +11,33 @@ describe('CardsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardsController],
-      providers: [CardsService],
+      providers: [
+        {
+          provide: CardsService,
+          useValue: {
+            create: jest
+              .fn<Promise<CreateCardDto & { id: number }>, [CreateCardDto]>()
+              .mockImplementation((cardDto) =>
+                Promise.resolve({ id: cardId, ...cardDto }),
+              ),
+            findOne: jest
+              .fn<Promise<CreateCardDto & { id: number }>, [number]>()
+              .mockImplementation((id) =>
+                Promise.resolve({
+                  id,
+                  name: 'Test SuperPokemon',
+                  pokemonType: 'DARK',
+                  evolutionType: 'STAGE1',
+                  attack: { name: 'Great attack', damageAmount: 50 },
+                  hitPoints: 70,
+                  weakness: { type: 'COLORLESS', times: 2 },
+                  resistance: { type: 'GRASS', value: 90 },
+                  rarity: 'RARE',
+                }),
+              ),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CardsController>(CardsController);
@@ -16,5 +45,41 @@ describe('CardsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create card', () => {
+    it('should create a new card', async () => {
+      const newCardDto: CreateCardDto = {
+        name: 'Test SuperPokemon',
+        pokemonType: 'DARK',
+        evolutionType: 'STAGE1',
+        attack: { name: 'Great attack', damageAmount: 50 },
+        hitPoints: 70,
+        weakness: { type: 'COLORLESS', times: 2 },
+        resistance: { type: 'GRASS', value: 90 },
+        rarity: 'RARE',
+      };
+
+      await expect(controller.create(newCardDto)).resolves.toEqual({
+        id: cardId,
+        ...newCardDto,
+      });
+    });
+
+    describe('get card by id', () => {
+      it('should get a single card', async () => {
+        await expect(controller.findOne('15')).resolves.toEqual({
+          id: 15,
+          name: 'Test SuperPokemon',
+          pokemonType: 'DARK',
+          evolutionType: 'STAGE1',
+          attack: { name: 'Great attack', damageAmount: 50 },
+          hitPoints: 70,
+          weakness: { type: 'COLORLESS', times: 2 },
+          resistance: { type: 'GRASS', value: 90 },
+          rarity: 'RARE',
+        });
+      });
+    });
   });
 });
